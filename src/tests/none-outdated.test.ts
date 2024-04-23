f14a0fc909bbb872bd2ab7d393662804b2a087b8
Lily Skye <me@suchipi.com>
2024-04-23 00:41:47 -0600

    add "none outdated" test

---
+++ src/tests/none-outdated.test.ts
@@ -0,0 +1,43 @@
+import fs from "node:fs";
+import { test, beforeEach, expect } from "vitest";
+import { spawn } from "first-base";
+import { fixturesDir, workDirs, cleanResult } from "./test-utils";
+
+const ownWorkDir = workDirs.concat("none-outdated");
+
+beforeEach(() => {
+  if (fs.existsSync(ownWorkDir.toString())) {
+    fs.rmSync(ownWorkDir.toString(), { recursive: true });
+  }
+  fs.mkdirSync(ownWorkDir.toString(), { recursive: true });
+});
+
+test("handles correct snapshots appropriately", async () => {
+  const sourceFixturePath = fixturesDir.concat("none-outdated.js");
+  const ownFixturePath = ownWorkDir.concat("none-outdated-1.js");
+
+  fs.copyFileSync(sourceFixturePath.toString(), ownFixturePath.toString());
+
+  const contentBefore = fs.readFileSync(ownFixturePath.toString(), "utf-8");
+
+  const run = spawn("node", [
+    "-r",
+    fixturesDir.concat("configs/make-library-global.js").toString(),
+    ownFixturePath.toString(),
+  ]);
+  await run.completion;
+
+  const contentAfter = fs.readFileSync(ownFixturePath.toString(), "utf-8");
+
+  expect(cleanResult(run.result)).toMatchInlineSnapshot(`
+    {
+      "code": 0,
+      "error": false,
+      "stderr": "",
+      "stdout": "",
+    }
+  `);
+
+  // Doesn't update snapshot
+  expect(contentBefore).toBe(contentAfter);
+});
